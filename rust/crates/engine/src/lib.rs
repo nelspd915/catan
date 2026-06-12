b9b0bf17f7652a23bfa085f19e60c2b8dac744ec
Jackalope <nelspd915@gmail.com>
2026-06-12 09:45:02 -0700

    fill in more structural outline to the engine

--- rust/crates/engine/src/lib.rs
+++ rust/crates/engine/src/lib.rs
@@ -13,10 +13,21 @@ pub struct Board {
     pub robber: TileId,
 }
 
+pub struct TileId(u8);
+
 pub struct Tile {
     pub terrain: Terrain,
     pub number: Option<u8>,
     pub nodes: [NodeId; 6],
+}
+
+pub enum Terrain {
+    Desert,
+    Hills,
+    Forest,
+    Mountains,
+    Fields,
+    Pasture,
 }
 
 pub struct Node {
@@ -26,7 +37,50 @@ pub struct Node {
     pub building: Option<Building>,
 }
 
+pub enum Port {
+    Brick,
+    Lumber,
+    Wool,
+    Grain,
+    Ore,
+    Generic,
+}
+
+pub struct EdgeId(u8);
+
+pub enum Building {
+    Settlement(PlayerId),
+    City(PlayerId),
+}
+
 pub struct Edge {
     pub nodes: [NodeId; 2],
     pub road: Option<Road>,
+}
+
+pub struct Road {
+    pub owner: PlayerId,
+}
+
+pub struct NodeId(u8);
+
+pub struct PlayerId(u8);
+
+pub struct PlayerState {
+    id: PlayerId,
+    resources: ResourceCounts,
+    roads_remaining: u8,
+    settlements_remaining: u8,
+    cities_remaining: u8,
+    victory_points: u8,
+    longest_road: u8,
+    army_size: u8,
+}
+
+pub struct ResourceCounts {
+    pub brick: u8,
+    pub lumber: u8,
+    pub wool: u8,
+    pub grain: u8,
+    pub ore: u8,
 }
